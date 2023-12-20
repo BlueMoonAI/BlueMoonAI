3d7fd4b56a61de1335afc54622faca4609cafaeb
Kazuki Asuna <138202531+KazukiAsuna@users.noreply.github.com>
2023-12-20 23:51:01 +0530

    Update

--- ui/javascript/analytics.js
+++ ui/javascript/analytics.js
@@ -1,15 +1,21 @@
 // analytics.js
 
-// Google tag (gtag.js)
-(function() {
-  var script = document.createElement('script');
-  script.async = true;
-  // for performance and seo testing
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-MND4E5TGVG';
-  document.head.appendChild(script);
+// Clarity only for research purposes reachability
+(function(c, l, a, r, i, t, y){
+    c[a] = c[a] || function(){(c[a].q = c[a].q || []).push(arguments)};
+    t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i + "?ref=bwt";
+    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "k8zqi9mv1g");
 
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-MND4E5TGVG');
+// Google Analytics only for research purposes reachability
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-MND4E5TGVG';
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-MND4E5TGVG');
 })();
