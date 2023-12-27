beaeccd380d10f34a83cc675537dbe7682b2a0b1
KazukiAsuna <138202531+KazukiAsuna@users.noreply.github.com>
2023-12-27 19:29:48 +0530

    improved downloads UI

--- modules/download_models.py
+++ modules/download_models.py
@@ -3,5 +3,5 @@ from bluemoon.utils.logly import logly
 
 def start_download(selected_model):
     logly.info('Starting download'
-           ' of model: ' + selected_model['name'] + ' ')
+           ' of model: ' + selected_model + ' ')
 
