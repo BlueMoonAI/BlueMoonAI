d64735a1a62c9eb2250dba8645da91c297debdfd
MorikawaAsami <138022882+MorikawaAsami@users.noreply.github.com>
2024-01-08 14:30:13 +0530

    fixed the metadata.json not found error

--- modules/metadata.py
+++ modules/metadata.py
@@ -1,14 +1,18 @@
 import json
 
 from bluemoon.utils.logly import logly
-
+import os
 
 def save_metadata(filename, data):
     try:
-        # Save each key-value pair separately for the image
-        with open(filename, 'a+') as f:
-            for key, value in data:
+        # Check if the file exists
+        file_exists = os.path.isfile(filename)
+
+        # Open the file in append mode if it exists; otherwise, open it in write mode to create a new file
+        with open(filename, 'a+' if file_exists else 'w+') as f:
+            for key, value in data.items():
                 json.dump({key: value}, f, indent=2)
                 f.write('\n')
     except Exception as e:
         logly.error('Error while logging metadata: {}'.format(e))
+
